dc93c137177a5e638ef7119131780fa721c1a3fe
metalgearalex <alex.chavdaroff@gmail.com>
2015-10-15 21:19:07 -0400

    stackoverflow/official JDBC code; just added some more comments and
    explanations really

--- RestWebService/src/javaWebService/ConnectingToPostgreSQL.java
+++ RestWebService/src/javaWebService/ConnectingToPostgreSQL.java
@@ -5,15 +5,13 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+
 public class ConnectingToPostgreSQL {
 
 	//if openConnection has an issue, see this: http://alvinalexander.com/java/jdbc-connection-string-mysql-postgresql-sqlserver
 	//actually just use syntax from the actual heroku site 
     static String openConnection = "jdbc:postgresql://<ec2-54-227-253-238.compute-1.amazonaws.com>:<5432>/<d20hohh2uq3tib>?user=<lqwgfavqzszazx>&password=<dYfO-uBInG-WGgkqGFpz4GHAjc>&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"; 
     private static String connectionAnswer; //seems like this should be elsewhere or is just a duplicate in general? static String connectionAnswer= "empty";  	
-    
-
-    
     
     
     public String getSQLData() {
@@ -25,8 +23,8 @@ public class ConnectingToPostgreSQL {
         Class.forName("org.postgresql.Driver"); //Before you can connect to a database, you need to load the driver. This is one of the 2 standard ways
         connection = DriverManager.getConnection(openConnection); //used to get a Connection instance from JDBC; global JDBC method
         statement = connection.createStatement(); //Any time you want to issue SQL statements to the database, you require a Statement or PreparedStatement instance.
-        String querySQL = "SELECT price* FROM ApartmentInventory"; //"SELECT attribute * FROM tableName WHERE condition", Example: “SELECT price FROM ApartmentListDatabaseTable WHERE price < 4000”
-        result = statement.executeQuery(querySQL); //result is an instance of ResultSet. once you issue your query it returns a ResultSet instance which contains the entire result
+        String querySQL = "select price " + "from ApartmentInventory";//"SELECT price* FROM ApartmentInventory"; //"SELECT attribute * FROM tableName WHERE condition", Example: “SELECT price FROM ApartmentListDatabaseTable WHERE price < 4000”
+        result = statement.executeQuery(querySQL); //result is an instance of ResultSet. executeQuery returns a ResultSet instance which contains the entire result
         //note that by default the Driver collects all the results for the query at once. You can make it fetch only a few rows however using cursors 
         connectionAnswer = "";
         while (result.next()) { //before reading any values you must call next() This returns true if there is a result, but more importantly, it prepares the row for processing.
@@ -70,8 +68,9 @@ public class ConnectingToPostgreSQL {
     	}//should match up to end of this method
     
     public String getAnswerSql (){
-        //execute(); >caused an error 
+        //execute(); //>from stackoverflow but not in official oracle docs, caused an error and seems uneccessary just returns true or false
         return connectionAnswer;
     		}
+    
     
 }//end of ConnectingToPostgreSQL class    
